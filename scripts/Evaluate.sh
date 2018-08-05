52f2b3dfd7326e2dafbc7daae524af96fe36567d
zhaoyong <zd5945@126.com>
2018-08-05 13:42:17 +0800

    modified evaluate

--- scripts/Evaluate.sh
+++ scripts/Evaluate.sh
@@ -140,7 +140,7 @@ if [ -n "$1" ];then
 		-h)     shift 1;echo_help;exit 1;;                   #Show usages 
 		-i)     shift 1;echo_introduction;exit 1;;           #Show introduction 
 		-edit)  shift 1;gedit $Here_Path/$File_Name;exit 1;; #Edit this function 
-		-e)     shift 1;evaluate $*;exit 1;; #Evaluate
+		-e)     shift 1;evaluate $*;exit 0;; #Evaluate
 		-*)     echo "error: no such option $1. -h for help";exit 1;; 
 		*)      $*;exit 1;;                                  #Call function here
 ##END_HELP##
