2c52b86a6ce9717c2f551b4e8da0481701a216cb
zhaoyong <zd5945@126.com>
2018-08-05 17:02:36 +0800

    modified evaluation

--- scripts/Evaluate.sh
+++ scripts/Evaluate.sh
@@ -108,6 +108,7 @@ evaluate() ## do evaluation
     fi
   done < $INPUTFILE
 
+  echo $TOPLINE
   echo $TOPLINE >> $OUTPUTFILE
   echo $SECONDLINE >>$OUTPUTFILE
 
@@ -120,6 +121,7 @@ evaluate() ## do evaluation
     done
 
     echo $LINE >>$OUTPUTFILE
+    echo $LINE
   done
 
   # Compare two files
@@ -143,6 +145,7 @@ if [ -n "$1" ];then
 		-i)     shift 1;echo_introduction;exit 1;;           #Show introduction 
 		-edit)  shift 1;gedit $Here_Path/$File_Name;exit 1;; #Edit this function 
 		-e)     shift 1;evaluate $*;exit 0;; #Evaluate
+		-a)     shift 1;AbortUpdate=YES;; #Evaluate
 		-*)     echo "error: no such option $1. -h for help";exit 1;; 
 		*)      $*;exit 1;;                                  #Call function here
 ##END_HELP##
--- tool/linux/evaluation/evaluate.sh
+++ tool/linux/evaluation/evaluate.sh
@@ -17,11 +17,97 @@ else
 	Function_Name=${File_Name%.*};
 fi
 
-echo evaluating topic $topic of $name
-
 if [ ! -f "$topicFolder/$name/README.md" ];then
     SCORE="D"
     return
 fi
 
-SCORE="[A]($topic/$name/README.md)"
+if [ ! -f "$topicFolder/$name/Statistics.sh" ];then
+    SCORE="D"
+    return
+fi
+
+testFolder=$Call_Path/tool/linux/$name
+mkdir -p $testFolder
+cp -f $topicFolder/$name/Statistics.sh $testFolder/Statistics.sh
+rm -f $testFolder/README.md
+output="$(bash $testFolder/Statistics.sh -e $topicFolder)"
+
+if [ ! -f "$testFolder/README.md" ];then # Failed to output README.md
+  echo "Failed to output $testFolder/README.md"
+  SCORE="C"
+  return
+fi
+
+
+difference="$(diff -q $testFolder/README.md $topicFolder/$name/README.md)"
+if [ -n "$difference" ]; then
+  echo "File $topicFolder/$name/README.md are not created by $testFolder/Statistics.sh, diff: \"$difference\""
+  SCORE="C";return
+fi
+
+# Check output correct or not
+
+  INPUTFILE=$topicFolder/README.md
+  OUTPUTFILE=$testFolder/DEMO.txt
+
+  TopicReg='\|[ ]*\[([^ ]+)\]'
+  NameReg='\|[ ]*([^ ]+)[ ]*\|[ ]*[0-9]+'
+  while read line
+    do  
+    if [[ $line =~ $TopicReg ]];then
+        EXISTED=
+        for topic in $Topics;do
+          if [ "${BASH_REMATCH[1]}" = "$topic" ];then EXISTED=YES;fi
+        done
+        if [ -z "$EXISTED" ];then
+          Topics="$Topics ${BASH_REMATCH[1]}"
+        fi
+    elif [[ $line =~ $NameReg ]];then
+      Names="$Names ${BASH_REMATCH[1]}"
+    fi
+  done < $INPUTFILE
+  
+  TOPLINE="\| *Topic *|"
+  for name in $Names;do
+    TOPLINE="$TOPLINE *$name *\|"
+  done
+
+checkline()
+{
+  topic="${Topics[$1 - 2]}"
+  LINE="\| *$topic *|"
+  for name in $Names;do
+    if [ -f "$1/$topic/$name/README.md" ];then
+        CONTENT=$(cat $1/$topic/$name/README.md)
+        LINE="$LINE *$CONTENT *| *"
+      else
+        LINE="$LINE *D *| *"
+      fi
+  done
+  echo $LINE
+}
+
+lineID=0;
+maxLineSize=2
+for topic in $Topics;do maxLineSize=$(expr $maxLineSize + 1);done
+
+while read line ; do
+  if [ $lineID -ge $maxLineSize ]; then
+    echo "lineID $lineID ($line) should not greater than $maxLineSize"
+  #  SCORE="B";return
+  fi
+  reg=""
+  case "$lineID" in
+    "0") reg="$TOPLINE";; 
+    "1") reg="";;
+    "*") reg="$(checkline $lineID $line)";;
+  esac
+  lineID=$(expr $lineID + 1)
+  if [[ ! $line =~ $reg ]];then
+    echo "$line not match $reg"
+    SCORE="B";return
+  fi
+done < $testFolder/README.md
+
+SCORE="[S]($topic/$name/README.md)"
