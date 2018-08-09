506e8736996d3d5abd3580ff2bd61f8dc1914b47
crischeng <420985011@qq.com>
2018-08-10 00:01:13 +0800

    add helloc and hellopython by cris

---
+++ cpp/helloc/程宇琪/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int findmax(int nu, int num, double* Arr)
+{
+
+    int indel = 0.5*num;
+    if( Arr[indel] > Arr[indel+1] && Arr[indel] > Arr[indel-1] )
+        return nu+indel;
+    else if (Arr[indel] < Arr[indel+1])
+        return findmax(nu+indel,num-indel , &Arr[indel]);
+    else
+        return findmax(nu,num-indel, Arr);
+
+
+}
+
+int main (int argc ,char** argv )
+{
+
+    double* arr = (double*)malloc(argc * sizeof(double));
+    for(int i = 1; i < argc; ++i)
+        arr[i-1] = atof ( argv[i]);
+    printf("%d",findmax(0,argc , arr));
+
+    return 0;
+}
