e51469a4df1120d397970cae65d089b544c412fb
tongpinmo <1529901761@qq.com>
2018-08-11 01:54:50 +0800

    python module

---
+++ python/pythonmodule/tongpinmo/test_package.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python27
+#coding:utf-8
+
+from person_package.data_analysis import DataAnalysis
+
+
+filename = 'data.txt'
+
+analysis=DataAnalysis(filename)
+
+I=analysis.plotXY()
+
+subseq = analysis.findMaxSubSequence()
+
+if subseq == [1, 2, 8, 9, 10, 11, 13]:
+    print('PASS')
+else:
+    print('FAILURE')
+
+
+
+
+
+
+
+
+
