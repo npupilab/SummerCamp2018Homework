5a2fc2ab1fa243827f346519b308f031404270c4
JiaoYanMoGu <1224830613@qq.com>
2018-08-13 21:54:17 +0800

    add cmake homework

--- tool/cmake/徐磊/FindHello.cmake
+++ tool/cmake/徐磊/FindHello.cmake
@@ -1,12 +1,17 @@
-find_path(Hello_INCLUDE_DIR Hello.h ${PROJECT_SOURCE_DIR} 
-	 \usr\include
-	 \usr\local\include)
-
- if(Hello_INCLUDE_DIR and  Hello_LIBRARIES )
-	message(STATUS "Looking for Hello lib  - found")
+find_path(Hello_INCLUDE_DIR hello/hello.h ${PROJECT_SOURCE_DIR}/../include)
+find_library(Hello_LIBRARIES hello  ${PROJECT_SOURCE_DIR}/../lib)
+if(Hello_INCLUDE_DIR AND  Hello_LIBRARIES)
+    message(STATUS "Looking for Hello lib  - found ${Hello_LIBRARIES}")
 	message(STATUS "Hello lib include path: ${Hello_INCLUDE_DIR}")
 	set(Hello_FOUND 1)
-else
+else(Hello_INCLUDE_DIR AND  Hello_LIBRARIES)
+    message(STATUS "Can not Find Hello lib")
+    set(Hello_FOUND 0)
+endif(Hello_INCLUDE_DIR AND  Hello_LIBRARIES)
+
+message("test")
+
+
 
 	
 	
