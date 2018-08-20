f2995375da106b8fc919459f27ac7a6709eefac2
zhaoyong <zd5945@126.com>
2018-08-20 20:15:05 +0800

    evaluate opt_nolinear

--- slam/opt_nolinear/README.md
+++ slam/opt_nolinear/README.md
@@ -1,5 +1,23 @@
 # Exercise of slam/opt_nolinear
 
-请使用LM算法，完成solvePnP函数并使测试通过。
+请使用LM算法，继承GSLAM::Optimizer并实现optimizePnP函数，使程序编译通过并获得S。
 
+
+## 接口说明
+
+```
+// Update pose with 3D-2D corrospondences
+
+  virtual bool optimizePnP(
+      const std::vector<std::pair<GSLAM::Point3d, CameraAnchor> >& matches,    // 3D points and projected point
+      GSLAM::SE3& pose, KeyFrameEstimzationDOF dof = UPDATE_KF_SE3,            // Pose input and output
+      double* information = NULL);
+
+```
+
+
+### 注意事项
+
+1. 脱离内置PICMake编译时，请pull更新GSLAM版本（为了保持与作业内置GSLAM一致）
+2. 实现类请使用USE_OPTIMIZER_PLUGIN注册，具体可参考赵勇实现
 
---
+++ slam/opt_nolinear/evaluation/evaluate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#####################  Build in Things   ######################
+#. $Function_Top/Include/Enviroment_Config.inc
+Call_Path=$(pwd) #Where this call from?
+#Where is here?
+if [ -n "$Function_Path" ];then
+	Here_Path=$Function_Path
+else
+	Here_Path=$(dirname $(readlink -f $0))
+fi
+topicFolder=$Here_Path/..
+topic=$1
+name=$2
+BuildDir=$Call_Path/$topic/$name
+scoreFile=$3
+
+mkdir -p $BuildDir
+
+if [ ! -d "$topicFolder/$name" ];then
+    echo "[D]($topic/evaluation/none.md)"> "$scoreFile"
+    exit 0
+fi
+
+cd $BuildDir
+INSTALL_DIR=$BuildDir/installFolder
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release $topicFolder/src -DUSER_NAME=$name
+make
+cd $Call_Path
+
+if [ ! -f "$BuildDir/solvePnP" ];then
+    echo "[C]($topic/evaluation/compile_failed.md)"> "$scoreFile"
+    exit 0
+fi
+
+$BuildDir/solvePnP $topic $name $scoreFile
+
+
+if [ ! -f "$scoreFile" ];then
+    echo "[C]($topic/evaluation/run_failed.md)"> "$scoreFile"
+    exit 0
+fi
