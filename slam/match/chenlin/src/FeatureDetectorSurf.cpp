0b1922eb43e17945a0e81dd70605669abddcf75c
npu-chenlin <444267416@qq.com>
2018-11-06 14:49:47 +0800

    add nonfree in slam/match

--- slam/match/chenlin/src/FeatureDetectorSurf.cpp
+++ slam/match/chenlin/src/FeatureDetectorSurf.cpp
@@ -2,7 +2,7 @@
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
+#include "nonfree/nonfree.hpp"
 #include <opencv2/legacy/legacy.hpp>
 
 #include "FeatureDetector.h"
